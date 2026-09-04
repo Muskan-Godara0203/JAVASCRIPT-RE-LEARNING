f78a9ca9bf78691fdbfaa0d0debaf52c64483efe
muskangotnochill <muskangodara0203@gmail.com>
2026-09-04 16:30:25 +0530

    Initial commit

---
+++ day1.js
@@ -0,0 +1,17 @@
+// console.log("Hello");
+
+// let name = "Muskan";
+// console.log(name);
+
+// let age = 19;
+// let surname = "Godara";
+// console.log(age);
+// console.log(surname);
+
+// let age = 19;
+// age = 20;
+// console.log(age);
+
+const name = "Muskan";
+// name = "Alex";
+console.log(name);
