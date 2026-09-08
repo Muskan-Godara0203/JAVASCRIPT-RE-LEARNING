735462e0ee01dd4421d95ef8c5e96ad967c8b4b9
muskangotnochill <muskangodara0203@gmail.com>
2026-09-08 19:55:59 +0530

    Started Day5

---
+++ day5.js
@@ -0,0 +1,24 @@
+let moviesex = ["Inception", "Interstellar", "The Dark Knight", "Parasite", "whiplash"];
+
+// Creating Arrays
+let fruits = ["apple", "Banana", "Mango"];
+
+// Array constructor (rarely used, but exists)
+let numbers = new Array(1, 2, 3);
+
+// Assessing Elements by Index
+let movies2 = ["Inception", "Interstellar", "The Dark Knight", "Parasite", "Whiplash"];
+console.log(movies2[0]);
+console.log(movies2[2]);
+
+console.log(movies2[movies2.length - 1]); // use ".LENGTH - 1" to get the last element
+
+// ".PUSH()" Method
+movies2.push("The Matrix");
+console.log(movies2);
+console.log(movies2.length);
+
+// Exercise: Movie Array
+// Task: Make an array of 5 movies. Log the first, the last, and add a 6th.
+
+let movies = ["Inception", "Interstellar", "The Dark Knight", "Parasite", ]
